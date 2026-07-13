479e0ba5dcfc056db25d71905b266d9d08d271bd
bkd <contact@bkd.lol>
2026-07-13 18:28:35 -0500

    init

---
+++ src/main/java/lol/bkd/deathcount/Deathcount.java
@@ -0,0 +1,16 @@
+package lol.bkd.deathcount;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Deathcount extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
